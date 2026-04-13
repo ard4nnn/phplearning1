f4b28dce56f63083a2b10feabae059f6c68090c1
ard4nnn <ard4nnn@gmail.com>
2026-04-13 12:10:11 +0800

    php first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "Evril";
+$umur = 20;
+$tinggi = 177.9;
+$menikah = false;
+$hobi = ["membaca novel dan manhwa","bermain game"];
+
+echo "nama saya $nama, umur saya $umur tahun, tinggi saya $tinggi cm, saya belum menikah, dan hobi saya adalah $hobi[0], dan $hobi[1]";
+?>
