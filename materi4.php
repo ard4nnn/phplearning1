4cfb54e81120c3375ee0f3522273dd2a13bf57dc
ard4nnn <ard4nnn@gmail.com>
2026-05-05 10:31:30 +0800

    5/5/2026

--- materi4.php
+++ materi4.php
@@ -25,3 +25,39 @@ if (isset($_POST['username'])) {
     Email : <input type="email" name="email"><br><br>
     <input type="submit" value="Kirim data">
 </form>
+
+<table border = "1">
+    <tr>
+        <th>Id</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Aksi</th>
+    </tr>
+
+<?php
+$sql = "SELECT * FROM user";
+$query = mysqli_query($koneksi, $sql);
+while ($row = mysqli_fetch_assoc($query)) {
+    echo"<tr>";
+    echo"<td>" . $row['id'] . "</td>";
+    echo"<td>" . $row['username'] . "</td>";
+    echo"<td>" . $row['password'] . "</td>";
+    echo"<td>" . $row['nama'] . "</td>";
+    echo"<td>" . $row['email'] . "</td>";
+    echo"<td> <a href='materi4.php?edit=$row[id]'>Edit</a> | <a href='materi4.php?hapus=$row[id]'>Hapus</a> </td>";
+    echo"</tr>";
+}
+if (isset($_GET['hapus'])) {
+    $id = $_GET['hapus'];
+    $sql = "DELETE FROM user WHERE id = $id";
+    $query = mysqli_query($koneksi, $sql);
+    if ($query) {
+        echo "Data berhasil dihapus";
+    } else {
+        echo "Data gagal dihapus";
+    }
+}
+?>
+</table>
